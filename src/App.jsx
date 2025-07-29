21ff308d59daf9ceddada1c50a8e5e3f561b7f4a
a71353 <goncalocafi@gmail.com>
2025-07-29 18:14:03 +0100

    standings component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Header from './components/Header'
 import Matches from './components/Matches'
+import Standings from './components/Standings'
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
         <Header />
-        <Matches />
+        <Routes>
+          <Route path='/' element={<Matches />} />
+          <Route path='/standings' element={<Standings />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
