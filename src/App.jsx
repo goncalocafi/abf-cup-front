17851754916df02385233f88e4e6417f36e0aaf2
a71353 <goncalocafi@gmail.com>
2025-07-29 14:32:35 +0100

    matches component css styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from './components/Header'
 import Matches from './components/Matches'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Matches />
+        <Header />
+        <Matches />
     </>
   )
 }
--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -1,24 +1,27 @@
-import img from "../assets/logo.png"
+import porto from "../assets/porto.png"
+import slb from "../assets/slb.png"
+import sporting from "../assets/sporting.png"
+import imortal from "../assets/imortal.png"
 
 export default function Matches() {
     return (
         <section className="matches">
+            <h2 className="match-date">Domingo 12 junho</h2>
             <div className="card">
-                <h2 className="match-date">Domingo 12 junho</h2>
                 <div className="match-score">
                     <div className="team home">
-                        <h3>sporting</h3>
-                        <img src={img} />
+                        <h3>Imortal</h3>
+                        <img src={imortal} />
                     </div>
 
                     <div className="score">
-                        <h3>0</h3>
+                        <h3>4</h3>
                         <p>FIM</p>
-                        <h3>0</h3>
+                        <h3>1</h3>
                     </div>
 
                     <div className="team away">
-                        <img src={img} />
+                        <img src={porto} />
                         <h3>Porto</h3>
                     </div>
                 </div>
@@ -27,23 +30,23 @@ export default function Matches() {
                     <p>Fase de grupos</p>
                 </div>
             </div>
+            <h2 className="match-date">Sábado 11 junho</h2>
             <div className="card">
-                <h2 className="match-date">Domingo 12 junho</h2>
                 <div className="match-score">
                     <div className="team home">
-                        <h3>sporting</h3>
-                        <img src={img} />
+                        <h3>Benfica</h3>
+                        <img src={slb} />
                     </div>
 
                     <div className="score">
-                        <h3>0</h3>
+                        <h3>19</h3>
                         <p>FIM</p>
                         <h3>0</h3>
                     </div>
 
                     <div className="team away">
-                        <img src={img} />
-                        <h3>Porto</h3>
+                        <img src={sporting} />
+                        <h3>Sporting</h3>
                     </div>
                 </div>
                 <div className="match-info">
@@ -54,18 +57,18 @@ export default function Matches() {
             <div className="card">
                 <div className="match-score">
                     <div className="team home">
-                        <h3>sporting</h3>
-                        <img src={img} />
+                        <h3>Benfica</h3>
+                        <img src={slb} />
                     </div>
 
                     <div className="score">
-                        <h3>0</h3>
+                        <h3>5</h3>
                         <p>FIM</p>
                         <h3>0</h3>
                     </div>
 
                     <div className="team away">
-                        <img src={img} />
+                        <img src={porto} />
                         <h3>Porto</h3>
                     </div>
                 </div>
