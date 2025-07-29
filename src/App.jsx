bfc8d0e2e5ab34333b989230fa54064cdaebc4d9
a71353 <goncalocafi@gmail.com>
2025-07-29 13:02:56 +0100

    matches component with fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Matches from './components/Matches'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Matches />
     </>
   )
 }
